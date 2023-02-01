687d634d2f72727c273299a9b4fbfd129e252923
123456kajal <121849532+123456kajal@users.noreply.github.com>
2023-02-01 23:21:14 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,104 @@
+<?php
+include 'db-connect.php';
+$searchErr = '';
+$student_details='';
+if(isset($_POST['save']))
+{
+    if(!empty($_POST['search']))
+    {
+        $search = $_POST['search'];
+        $stmt = $con->prepare("select * from student where TID like '%$search%'");
+        $stmt->execute();
+        $employee_details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //print_r($student_details);
+         
+    }
+    else
+    {
+        $searchErr = "Please enter the information";
+    }
+    
+}
+ 
+?>
+<html>
+<head>
+<title>ajax example</title>
+<link rel="stylesheet" href="bootstrap.css" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="bootstrap-theme.css" crossorigin="anonymous">
+<style>
+.container{
+    width:70%;
+    height:30%;
+    padding:20px;
+}
+</style>
+</head>
+ 
+<body>
+    <div class="container">
+    <h3><u>PHP MySQL search database and display results</u></h3>
+    <br/><br/>
+    <form class="form-horizontal" action="#" method="post">
+    <div class="row">
+        <div class="form-group">
+            <label class="control-label col-sm-4" for="email"><b>Search Employee Information:</b>:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" name="search" placeholder="search here">
+            </div>
+            <div class="col-sm-2">
+              <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
+            </div>
+        </div>
+        <div class="form-group">
+            <span class="error" style="color:red;">* <?php echo $searchErr;?></span>
+        </div>
+         
+    </div>
+    </form>
+    <br/><br/>
+    <h3><u>Search Result</u></h3><br/>
+    <div class="table-responsive">          
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>CRN</th>
+            <th>Name</th>
+            <th>Branch</th>
+            <th>TID</th>
+          </tr>
+        </thead>
+        <tbody>
+                <?php
+                 if(!$student_details)
+                 {
+                    echo '<tr>No data found</tr>';
+                 }
+                 else{
+                    foreach($student_details as $key=>$value)
+                    {
+                        ?>
+                    <tr>
+                        <td><?php echo $key+1;?></td>
+                        <td><?php echo $value['CRN'];?></td>
+                        <td><?php echo $value['Name'];?></td>
+                        <td><?php echo $value['Branch'];?></td>
+                        <td><?php echo $value['TID'];?></td>
+                    </tr>
+                         
+                        <?php
+                    }
+                     
+                 }
+                ?>
+             
+         </tbody>
+      </table>
+    </div>
+</div>
+<script src="jquery-3.2.1.min.js"></script>
+<script src="bootstrap.min.js"></script>
+</body>
+</html>
